d8f5922d79e8d1ac04ce4e820e208029ded19e76
Prakhar <prakharmishra2593@gmail.com>
2016-12-27 02:06:43 +0530

    latest commit

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 # Create your views here.
-from datetime import timedelta, date
+from datetime import timedelta, date , datetime
 import json
 
 try:
@@ -13,7 +14,7 @@ def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
-def getReading(single_date,place,parameter):
+def getReading(single_date,place,parameter,timeinterval):
 	x = []
 	y = []
 	place = urllib2.quote(place)
@@ -30,12 +31,21 @@ def getReading(single_date,place,parameter):
 		return x , y
 
 	#print("coming here")
-	observations = parsed_json['history']['observations']	
+	observations = parsed_json['history']['observations']
+
+	if timeinterval == '' and len(observations) > 1:
+		t1 = observations[0]['date']['hour'] + ":" + observations[0]['date']['min']
+		t2 = observations[1]['date']['hour'] + ":" + observations[1]['date']['min']
+		FMT = '%H:%M'
+		tdelta = datetime.strptime(t2, FMT) - datetime.strptime(t1, FMT)
+		d = datetime(1,1,1) + tdelta 
+		timeinterval =  str(d.day-1) + ":" + str(d.hour) + ":" + str(d.minute) + ":" + str(d.second)
+
 	for observation in observations:
 		x.append(observation['date']['pretty'])
 		y.append(observation[parameter])
 
-	return x , y
+	return x , y ,timeinterval
 
 def homepage(request):
 	x_axis = []
@@ -44,6 +54,7 @@ def homepage(request):
 	place = ''
 	sdate = ''
 	edate = ''
+	timeinterval = ''
 	parameter = 'dew'
 	if request.method == "POST":
 		cd = request.POST
@@ -57,11 +68,14 @@ def homepage(request):
 		parameter = cd["parameter"]
 		unitscale = cd['unitscale']
 		for single_date in daterange(start_date, end_date):
-		    x , y = getReading(single_date,place,parameter)
-		    x_axis = x_axis + x
-		    y_axis = y_axis + y
+			x , y , timeinterval = getReading(single_date,place,parameter,timeinterval)
+			if not x or not y:
+				messages.info(request, 'No data found for this city. Try a different name of your city.')
+				break
+			x_axis = x_axis + x
+			y_axis = y_axis + y
 
 		sdate = cd["start_date"]
 		edate = cd["end_date"]
 	z = zip(x_axis,y_axis)
-	return render(request,'weather/home.html',{'x' : x_axis , 'y' : y_axis ,'z' :z , 'unitscale' : unitscale,'place' : place,'sdate' : sdate,'edate' :edate , 'parameter' : parameter})
+	return render(request,'weather/home.html',{'x' : x_axis , 'y' : y_axis ,'z' :z , 'unitscale' : unitscale,'place' : place,'sdate' : sdate,'edate' :edate , 'parameter' : parameter , 'timeinterval' : timeinterval})
